4c72ba04abac23f942e74e21197257212355b751
Samuel Zaldivar <samzaldivar98@gmail.com>
2020-03-12 20:27:16 -0400

    Finished algorithm for selection sort

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,3 +1,23 @@
 export async function run(){
 
+  var array = [6,2,34,100,65,22,0,1,2,9];
+
+  var minIndex = 0;
+
+  for(var i = 0; i < array.length - 1; i++){
+
+    minIndex = i;
+
+    for(var j = i + 1; j < array.length; j++){
+
+      if(array[j] < array[minIndex]){
+        minIndex = j;
+      }
+    }
+
+    var temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+
+  }
 }
